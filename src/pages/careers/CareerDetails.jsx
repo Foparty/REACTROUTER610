130166f61927ff81fc4574a24e63013ff04fbd1d
Foparty <alfonsomartingarcia@gmail.com>
2023-04-29 09:10:47 +0200

    test github pages

--- src/pages/careers/CareerDetails.jsx
+++ src/pages/careers/CareerDetails.jsx
@@ -4,20 +4,20 @@ import { useLoaderData, useParams } from 'react-router-dom';
 import { careers } from '../../../public/data/data.js';
 
 const CareerDetails = () => {
-    const { id } = useParams();
-    const career = useLoaderData();
-    return (
-        <div className={styles.container}>
-            <h2>Career detail for: {career.title}</h2>
-        </div>
-    );
+  const { id } = useParams();
+  const career = useLoaderData();
+  return (
+    <div className={styles.container}>
+      <h2>Career detail for: {career.title}</h2>
+    </div>
+  );
 };
 export default CareerDetails;
 
 //loader function
 
 export const careerDetailsLoader = ({ params }) => {
-    const { id } = params;
-    const res = `${careers}/` + id;
-    return res;
+  const { id } = params;
+  const res = careers.find((id) => id === id);
+  return res;
 };
--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -4,22 +4,22 @@ import { useLoaderData, Link } from 'react-router-dom';
 import { careers } from '../../../public/data/data.js';
 
 const Careers = () => {
-    const career = useLoaderData();
-    return (
-        <div className={styles.careers}>
-            {career.map((item) => (
-                <Link to={item.id.toString()} key={item.id}>
-                    <p>{item.title}</p>
-                    <p>{item.location}</p>
-                </Link>
-            ))}
-        </div>
-    );
+  const career = useLoaderData();
+  return (
+    <div className={styles.careers}>
+      {career.map((item) => (
+        <Link to={item.id.toString()} key={item.id}>
+          <p>{item.title}</p>
+          <p>{item.location}</p>
+        </Link>
+      ))}
+    </div>
+  );
 };
 export default Careers;
 
-export const careersLoader = () => {
-    const response = careers;
+export const careersLoader = async () => {
+  const res = await fetch('https://foparty.github.io/data/careers');
 
-    return response;
+  return res.json();
 };
