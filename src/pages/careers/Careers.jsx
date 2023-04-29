052dbe960fc428019ab92e816b82d98c4c861f9a
Foparty <alfonsomartingarcia@gmail.com>
2023-04-29 09:59:32 +0200

    fdf

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -19,7 +19,7 @@ export default Careers;
 
 export const careersLoader = async () => {
   const res = await fetch(
-    'https://reactrouter-test.netlify.app/data/data.json/careers'
+    'https://reactrouter-test.netlify.app/data/data.json&careers'
   );
 
   return res.json();
